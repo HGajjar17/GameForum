ecb93e88ffbfc14cffc92de537ae129260efc5a0
Hiren Gajjar <144141125+HGajjar17@users.noreply.github.com>
2025-02-04 21:59:06 -0400

    CommentsController and Comments and Discussions working.

--- GameForum/Controllers/CommentsController.cs
+++ GameForum/Controllers/CommentsController.cs
@@ -19,31 +19,96 @@ namespace GameForum.Controllers
             _context = context;
         }
 
+        // Deleted the folowing action code
         // GET: Comments
-        public async Task<IActionResult> Index()
-        {
-            var gameForumContext = _context.Comment.Include(c => c.Discussion);
-            return View(await gameForumContext.ToListAsync());
-        }
+        // GET: Comments/Details/5
+        // GET: Comments/Edit/5
+        // POST: Comments/Edit/5
+
+
+
+        // GET: Comments
+        //public async Task<IActionResult> Index()
+        //{
+        //    var gameForumContext = _context.Comment.Include(c => c.Discussion);
+        //    return View(await gameForumContext.ToListAsync());
+        //}
 
         // GET: Comments/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var comment = await _context.Comment
-                .Include(c => c.Discussion)
-                .FirstOrDefaultAsync(m => m.CommentId == id);
-            if (comment == null)
-            {
-                return NotFound();
-            }
+        //    var comment = await _context.Comment
+        //        .Include(c => c.Discussion)
+        //        .FirstOrDefaultAsync(m => m.CommentId == id);
+        //    if (comment == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(comment);
-        }
+        //    return View(comment);
+        //}
+
+        // GET: Comments/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var comment = await _context.Comment.FindAsync(id);
+        //    if (comment == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ViewData["DiscussionId"] = new SelectList(_context.Discussion, "DiscussionId", "DiscussionId", comment.DiscussionId);
+        //    return View(comment);
+        //}
+
+        // POST: Comments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("CommentId,Content,CreateDate,DiscussionId")] Comment comment)
+        //{
+        //    if (id != comment.CommentId)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(comment);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!CommentExists(comment.CommentId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["DiscussionId"] = new SelectList(_context.Discussion, "DiscussionId", "DiscussionId", comment.DiscussionId);
+        //    return View(comment);
+        //}
+
+
+
+
 
         // GET: Comments/Create
         public IActionResult Create(int? id)
@@ -71,65 +136,14 @@ namespace GameForum.Controllers
             {
                 _context.Add(comment);
                 await _context.SaveChangesAsync();
-                // Redirect to the "Get Discussion" page (to be created in another task)
-                return RedirectToAction("Details", "Discussions", new { id = comment.DiscussionId });
+
+                // re-direct to /Discussions/Edit/{id}
+                return RedirectToAction("Edit", "Discussions", new { id = comment.DiscussionId });
             }
             ViewData["DiscussionId"] = new SelectList(_context.Discussion, "DiscussionId", "DiscussionId", comment.DiscussionId);
             return View(comment);
         }
 
-        // GET: Comments/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var comment = await _context.Comment.FindAsync(id);
-            if (comment == null)
-            {
-                return NotFound();
-            }
-            ViewData["DiscussionId"] = new SelectList(_context.Discussion, "DiscussionId", "DiscussionId", comment.DiscussionId);
-            return View(comment);
-        }
-
-        // POST: Comments/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CommentId,Content,CreateDate,DiscussionId")] Comment comment)
-        {
-            if (id != comment.CommentId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(comment);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CommentExists(comment.CommentId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["DiscussionId"] = new SelectList(_context.Discussion, "DiscussionId", "DiscussionId", comment.DiscussionId);
-            return View(comment);
-        }
 
         // GET: Comments/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -142,6 +156,7 @@ namespace GameForum.Controllers
             var comment = await _context.Comment
                 .Include(c => c.Discussion)
                 .FirstOrDefaultAsync(m => m.CommentId == id);
+
             if (comment == null)
             {
                 return NotFound();
@@ -155,14 +170,21 @@ namespace GameForum.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var comment = await _context.Comment.FindAsync(id);
+            var comment = await _context.Comment
+                .Include(c => c.Discussion)
+                .FirstOrDefaultAsync(m => m.CommentId == id);
+
             if (comment != null)
             {
                 _context.Comment.Remove(comment);
-            }
+                await _context.SaveChangesAsync();
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+                // re-direct to /Discussions/Edit/{id}
+                return RedirectToAction("Edit", "Discussions", new { id = comment.Discussion?.DiscussionId });
+
+            }
+            return View(comment);
+            //return RedirectToAction(nameof(Index));
         }
 
         private bool CommentExists(int id)
